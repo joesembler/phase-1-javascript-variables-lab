37330f80581bbd4a25bf24e1d144272f3bb89246
Joe Sembler <joeysembler@gmail.com>
2021-11-22 11:58:31 -0500

    commit tree 67e574364178e70e69ead6eb0aaa2de3cdf55e10

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+// Joe Sembler
+// Flatiron School
+// Phase 1: JavaScript Variables Lab
+
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
